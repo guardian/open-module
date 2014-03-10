a80da860f473e66ea010cbf68fb0b303a312d51c
Patrick Hamann <patrick.hamann@guardian.co.uk>
2014-03-10 15:49:01 +0000

    Add boot.js script

---
+++ static/boot.js
@@ -0,0 +1,97 @@
+/*
+ Copyright (c) 2008 Stefan Lange-Hegermann
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+function microAjax(url, callbackFunction)
+{
+    this.bindFunction = function (caller, object) {
+        return function() {
+            return caller.apply(object, [object]);
+        };
+    };
+
+    this.stateChange = function (object) {
+        if (this.request.readyState==4)
+            this.callbackFunction(this.request.responseText);
+    };
+
+    this.getRequest = function() {
+        if (window.ActiveXObject)
+            return new ActiveXObject('Microsoft.XMLHTTP');
+        else if (window.XMLHttpRequest)
+            return new XMLHttpRequest();
+        return false;
+    };
+
+    this.postBody = (arguments[2] || "");
+
+    this.callbackFunction=callbackFunction;
+    this.url=url;
+    this.request = this.getRequest();
+
+    if(this.request) {
+        var req = this.request;
+        req.onreadystatechange = this.bindFunction(this.stateChange, this);
+
+        if (this.postBody!=="") {
+            req.open("POST", url, true);
+            req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            req.setRequestHeader('Connection', 'close');
+        } else {
+            req.open("GET", url, true);
+        }
+
+        req.send(this.postBody);
+    }
+}
+
+
+/*
+ Guardian open module bootscript
+   This simply takes an open element generated from composer,
+   Gets the elements origin src, makes an ajax request to it, and replaces the element
+   with the return html.
+ */
+define('open-module', [], function() {
+
+   function boot(element, context, config, mediator) {
+       var isModernBrowser = 'querySelector' in document && (
+            ('XMLHttpRequest' in window && 'withCredentials' in new XMLHttpRequest())
+            || 'XDomainRequest' in window
+           );
+
+       if(!isModernBrowser) { return; }
+
+       var uri = element.getAttribute('data-src');
+
+       microAjax(uri, function(temp) {
+           var html = JSON.parse(temp).html;
+           var container = document.createElement('div');
+           container.innerHTML = html;
+           element.parentNode.replaceChild(container, element);
+       });
+   }
+
+   return {
+       boot: boot
+   }
+});
